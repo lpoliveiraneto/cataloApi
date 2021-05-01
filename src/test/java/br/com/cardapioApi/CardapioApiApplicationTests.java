65b77d6c29d381fe0bc778bfd5e7011cb7a2aaf2
lpoliveiraneto <leonidespires@gmail.com>
2021-05-01 18:37:44 -0300

    criando os pacotes para api

---
+++ src/test/java/br/com/cardapioApi/CardapioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cardapioApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardapioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
